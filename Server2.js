6a9e04e2238976e6272adaf7c41b9651aaecc2bc
dientc <trinhdien606@gmail.com>
2023-03-16 22:57:07 +0700

    lab 4

---
+++ Server2.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+//import { engine } from 'express-handlebars';
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+//app.engine('.hbs', ExpressHandlebars());
+app.engine(
+  ".hbs",
+  expressHbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: "views/layouts/",
+  })
+);
+
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("Bai1");
+});
+
+//app.use(express.static(__dirname));
+// app.get("/", function (req, res) {
+//     console.log(__dirname);
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
